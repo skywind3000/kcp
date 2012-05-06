09e1a3495db2595bfd8047bbad85e65579e6ba8b
skywind3000 <skywind3000@163.com>
2012-05-07 00:48:58 +0800

    immigrant from my original project http://code.google.com/p/ikcp

---
+++ test.cpp
@@ -0,0 +1,177 @@
+//=====================================================================
+//
+// test.cpp - kcp 测试用例
+//
+// 说明：
+// gcc test.cpp -o test -lstdc++
+//
+//=====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test.h"
+#include "ikcp.c"
+
+
+// 模拟网络
+LatencySimulator *vnet;
+
+// 模拟网络：模拟发送一个 udp包
+int udp_output(const char *buf, int len, ikcpcb *kcp, void *user)
+{
+	int id = (int)user;
+	vnet->send(id, buf, len);
+	return 0;
+}
+
+// 测试用例
+void test(int mode)
+{
+	// 创建模拟网络：丢包率10%，Rtt 60ms~125ms
+	vnet = new LatencySimulator(10, 60, 125);
+
+	// 创建两个端点的 kcp对象，第一个参数 conv是会话编号，同一个会话需要相同
+	// 最后一个是 user参数，用来传递标识
+	ikcpcb *kcp1 = ikcp_create(0x11223344, (void*)0);
+	ikcpcb *kcp2 = ikcp_create(0x11223344, (void*)1);
+
+	// 设置kcp的下层输出，这里为 udp_output，模拟udp网络输出函数
+	kcp1->output = udp_output;
+	kcp2->output = udp_output;
+
+	IUINT32 current = iclock();
+	IUINT32 slap = current + 20;
+	IUINT32 index = 0;
+	IUINT32 next = 0;
+	IINT64 sumrtt = 0;
+	int count = 0;
+	int maxrtt = 0;
+
+	// 配置窗口大小：平均延迟200ms，每20ms发送一个包，
+	// 而考虑到丢包重发，设置最大收发窗口为128
+	ikcp_wndsize(kcp1, 128, 128);
+	ikcp_wndsize(kcp2, 128, 128);
+
+	// 判断测试用例的模式
+	if (mode == 0) {
+		// 默认模式
+		ikcp_nodelay(kcp1, 0, 10, 0, 0);
+		ikcp_nodelay(kcp2, 0, 10, 0, 0);
+	}
+	else if (mode == 1) {
+		// 普通模式，关闭流控等
+		ikcp_nodelay(kcp1, 0, 10, 0, 1);
+		ikcp_nodelay(kcp2, 0, 10, 0, 1);
+	}	else {
+		// 启动快速模式
+		// 第二个参数 nodelay-启用以后若干常规加速将启动
+		// 第三个参数 interval为内部处理时钟，默认设置为 10ms
+		// 第四个参数 resend为快速重传指标，设置为2
+		// 第五个参数 为是否禁用常规流控，这里禁止
+		ikcp_nodelay(kcp1, 1, 10, 2, 1);
+		ikcp_nodelay(kcp2, 1, 10, 2, 1);
+	}
+
+
+	char buffer[2000];
+	int hr;
+
+	IUINT32 ts1 = iclock();
+
+	while (1) {
+		isleep(1);
+		current = iclock();
+		ikcp_update(kcp1, iclock());
+		ikcp_update(kcp2, iclock());
+
+		// 每隔 20ms，kcp1发送数据
+		for (; current >= slap; slap += 20) {
+			*(IUINT32*)(buffer + 0) = index++;
+			*(IUINT32*)(buffer + 4) = current;
+
+			// 发送上层协议包
+			ikcp_send(kcp1, buffer, 8);
+		}
+
+		// 处理虚拟网络：检测是否有udp包从p1->p2
+		while (1) {
+			hr = vnet->recv(1, buffer, 2000);
+			if (hr < 0) break;
+			// 如果 p2收到udp，则作为下层协议输入到kcp2
+			ikcp_input(kcp2, buffer, hr);
+		}
+
+		// 处理虚拟网络：检测是否有udp包从p2->p1
+		while (1) {
+			hr = vnet->recv(0, buffer, 2000);
+			if (hr < 0) break;
+			// 如果 p1收到udp，则作为下层协议输入到kcp1
+			ikcp_input(kcp1, buffer, hr);
+		}
+
+		// kcp2接收到任何包都返回回去
+		while (1) {
+			hr = ikcp_recv(kcp2, buffer, 10);
+			// 没有收到包就退出
+			if (hr < 0) break;
+			// 如果收到包就回射
+			ikcp_send(kcp2, buffer, hr);
+		}
+
+		// kcp1收到kcp2的回射数据
+		while (1) {
+			hr = ikcp_recv(kcp1, buffer, 10);
+			// 没有收到包就退出
+			if (hr < 0) break;
+			IUINT32 sn = *(IUINT32*)(buffer + 0);
+			IUINT32 ts = *(IUINT32*)(buffer + 4);
+			IUINT32 rtt = current - ts;
+			
+			if (sn != next) {
+				// 如果收到的包不连续
+				printf("ERROR sn %d<->%d\n", (int)count, (int)next);
+				return;
+			}
+
+			next++;
+			sumrtt += rtt;
+			count++;
+			if (rtt > maxrtt) maxrtt = rtt;
+
+			printf("[RECV] mode=%d sn=%d rtt=%d\n", mode, (int)sn, (int)rtt);
+		}
+		if (next > 1000) break;
+	}
+
+	ts1 = iclock() - ts1;
+
+	ikcp_release(kcp1);
+	ikcp_release(kcp2);
+
+	const char *names[3] = { "default", "normal", "fast" };
+	printf("%s mode result (%dms):\n", names[mode], ts1);
+	printf("avgrtt=%d maxrtt=%d\n", (int)(sumrtt / count), maxrtt);
+	printf("press enter to next ...\n");
+	char ch; scanf("%c", &ch);
+}
+
+int main()
+{
+	test(0);	// 默认模式，类似 TCP：正常模式，无快速重传，常规流控
+	test(1);	// 普通模式，关闭流控等
+	test(2);	// 快速模式，所有开关都打开，且关闭流控
+	return 0;
+}
+
+/*
+default mode result (20917ms):
+avgrtt=740 maxrtt=1507
+
+normal mode result (20131ms):
+avgrtt=156 maxrtt=571
+
+fast mode result (20207ms):
+avgrtt=138 maxrtt=392
+*/
+
---
+++ test.h
@@ -0,0 +1,241 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "ikcp.h"
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#include <windows.h>
+#elif !defined(__unix)
+#define __unix
+#endif
+
+#ifdef __unix
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#endif
+
+/* get system time */
+static inline void itimeofday(long *sec, long *usec)
+{
+	#if defined(__unix)
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	if (sec) *sec = time.tv_sec;
+	if (usec) *usec = time.tv_usec;
+	#else
+	static long mode = 0, addsec = 0;
+	BOOL retval;
+	static IINT64 freq = 1;
+	IINT64 qpc;
+	if (mode == 0) {
+		retval = QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+		freq = (freq == 0)? 1 : freq;
+		retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+		addsec = (long)time(NULL);
+		addsec = addsec - (long)((qpc / freq) & 0x7fffffff);
+		mode = 1;
+	}
+	retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+	retval = retval * 2;
+	if (sec) *sec = (long)(qpc / freq) + addsec;
+	if (usec) *usec = (long)((qpc % freq) * 1000000 / freq);
+	#endif
+}
+
+/* get clock in millisecond 64 */
+static inline IINT64 iclock64(void)
+{
+	long s, u;
+	IINT64 value;
+	itimeofday(&s, &u);
+	value = ((IINT64)s) * 1000 + (u / 1000);
+	return value;
+}
+
+static inline IUINT32 iclock()
+{
+	return (IUINT32)(iclock64() & 0xfffffffful);
+}
+
+/* sleep in millisecond */
+static inline void isleep(unsigned long millisecond)
+{
+	#ifdef __unix 	/* usleep( time * 1000 ); */
+	struct timespec ts;
+	ts.tv_sec = (time_t)(millisecond / 1000);
+	ts.tv_nsec = (long)((millisecond % 1000) * 1000000);
+	/*nanosleep(&ts, NULL);*/
+	usleep((millisecond << 10) - (millisecond << 4) - (millisecond << 3));
+	#elif defined(_WIN32)
+	Sleep(millisecond);
+	#endif
+}
+
+#ifdef __cplusplus
+#include <list>
+#include <vector>
+
+// 带延迟的数据包
+class DelayPacket
+{
+public:
+	virtual ~DelayPacket() {
+		if (_ptr) delete _ptr;
+		_ptr = NULL;
+	}
+
+	DelayPacket(int size, const void *src = NULL) {
+		_ptr = new unsigned char[size];
+		_size = size;
+		if (src) {
+			memcpy(_ptr, src, size);
+		}
+	}
+
+	unsigned char* ptr() { return _ptr; }
+	const unsigned char* ptr() const { return _ptr; }
+
+	int size() const { return _size; }
+	IUINT32 ts() const { return _ts; }
+	void setts(IUINT32 ts) { _ts = ts; }
+
+protected:
+	unsigned char *_ptr;
+	int _size;
+	IUINT32 _ts;
+};
+
+// 均匀分布的随机数
+class Random
+{
+public:
+	Random(int size) {
+		this->size = 0;
+		seeds.resize(size);
+	}
+
+	int random() {
+		int x, i;
+		if (seeds.size() == 0) return 0;
+		if (size == 0) { 
+			for (i = 0; i < (int)seeds.size(); i++) {
+				seeds[i] = i;
+			}
+			size = (int)seeds.size();
+		}
+		i = rand() % size;
+		x = seeds[i];
+		seeds[i] = seeds[--size];
+		return x;
+	}
+
+protected:
+	int size;
+	std::vector<int> seeds;
+};
+
+// 网络延迟模拟器
+class LatencySimulator
+{
+public:
+	virtual ~LatencySimulator() {
+		clear();
+	}
+
+	// lostrate: 往返一周丢包率的百分比，默认 10%
+	// rttmin：rtt最小值，默认 60
+	// rttmax：rtt最大值，默认 125
+	LatencySimulator(int lostrate = 10, int rttmin = 60, int rttmax = 125, int nmax = 1000): 
+		r12(100), r21(100) {
+		current = iclock();		
+		this->lostrate = lostrate / 2;	// 上面数据是往返丢包率，单程除以2
+		this->rttmin = rttmin / 2;
+		this->rttmax = rttmax / 2;
+		this->nmax = nmax;
+	}
+
+	// 清除数据
+	void clear() {
+		DelayTunnel::iterator it;
+		for (it = p12.begin(); it != p12.end(); it++) {
+			delete *it;
+		}
+		for (it = p21.begin(); it != p21.end(); it++) {
+			delete *it;
+		}
+		p12.clear();
+		p21.clear();
+	}
+
+	// 发送数据
+	// peer - 端点0/1，从0发送，从1接收；从1发送从0接收
+	void send(int peer, const void *data, int size) {
+		if (peer == 0) {
+			if (r12.random() < lostrate) return;
+			if ((int)p12.size() >= nmax) return;
+		}	else {
+			if (r21.random() < lostrate) return;
+			if ((int)p21.size() >= nmax) return;
+		}
+		DelayPacket *pkt = new DelayPacket(size, data);
+		current = iclock();
+		IUINT32 delay = rttmin;
+		if (rttmax > rttmin) delay += rand() % (rttmax - rttmin);
+		pkt->setts(current + delay);
+		if (peer == 0) {
+			p12.push_back(pkt);
+		}	else {
+			p21.push_back(pkt);
+		}
+	}
+
+	// 接收数据
+	int recv(int peer, void *data, int maxsize) {
+		DelayTunnel::iterator it;
+		if (peer == 0) {
+			it = p21.begin();
+			if (p21.size() == 0) return -1;
+		}	else {
+			it = p12.begin();
+			if (p12.size() == 0) return -1;
+		}
+		DelayPacket *pkt = *it;
+		current = iclock();
+		if (current < pkt->ts()) return -2;
+		if (maxsize < pkt->size()) return -3;
+		if (peer == 0) {
+			p21.erase(it);
+		}	else {
+			p12.erase(it);
+		}
+		maxsize = pkt->size();
+		memcpy(data, pkt->ptr(), maxsize);
+		return maxsize;
+	}
+
+protected:
+	IUINT32 current;
+	int lostrate;
+	int rttmin;
+	int rttmax;
+	int nmax;
+	typedef std::list<DelayPacket*> DelayTunnel;
+	DelayTunnel p12;
+	DelayTunnel p21;
+	Random r12;
+	Random r21;
+};
+
+#endif
+
+#endif
+
+
